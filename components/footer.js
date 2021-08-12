98e6fe6fdc050adbcd3b4b6770361c37257be76c
Leo Petrenko <mail@itart.pro>
2021-08-12 23:46:37 +0300

    fresh start

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,4 @@
-import {FaVk, FaFacebookSquare, FaTwitter, FaPhone} from 'react-icons/fa'
-import Link from 'next/link'
-
 export default function Footer() {
-
     return (
         <footer>Footer</footer>
     )
