e49c5df98f4ed250bdb787ad50904256676ea41c
Leo Petrenko <mail@itart.pro>
2021-08-12 23:46:08 +0300

    fresh start

--- components/public/public-layout.js
+++ components/public/public-layout.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import {useRouter} from 'next/router';
-import SideNav from './side-nav';
 import css from './public-layout.module.css';
 
 const PublicLayout = ({page, user, children, ogImage}) => {
@@ -22,16 +21,9 @@ const PublicLayout = ({page, user, children, ogImage}) => {
                 </>
                 }
                 <meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME}/>
-                <meta property="og:image" content={ogImage || "/images/logo.svg"}/>
+                <meta property="og:image" content={ogImage || "/images/logo.png"}/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:url" content={process.env.NEXT_PUBLIC_SITE_URL + asPath}/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#861039"/>
-                <meta name="msapplication-TileColor" content="#861039"/>
-                <meta name="theme-color" content="#861039"/>
             </Head>
             <div className={`row`}>
                 {user && <nav className={`col start ${css.n1}`}><SideNav user={user} /></nav>}
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
 import PublicLayout from 'components/public/public-layout'
-import Login from "../components/public/loginforms/login";
-import Register from "../components/public/loginforms/register";
 
 const Home = () => {
     const page = {
-        'title':'social network title',
-        'description':'social network description',
-        'keywords':'social network keywords',
+        'title':'',
+        'description':'',
+        'keywords':''
     };
-    const ogImage = 'https://itart.pro/images/splash.png';
+    const ogImage = '';
 
     return (
         <PublicLayout page={page} ogImage={ogImage}>
             <main className={`row center`}>
-                <h1>Логин и регистрация</h1>
-                <Login/>
-                <Register/>
+                <h1>Постройка</h1>
             </main>
         </PublicLayout>
     )
