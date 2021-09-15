92bd5339de5d00259458bab6328409269ffe393c
axlkov <ieffector@gmail.com>
2021-09-15 16:00:57 +0300

    formasters - adaptiv

--- pages/for-masters.js
+++ pages/for-masters.js
@@ -7,28 +7,23 @@ const ForMasters = () => {
         <PublicLayout>
             <main className={'row bet max center'}>
                 <div className={`col`}>
-                    <div className={`row`}>
+                    <div className={`row bet`}>
                         <div className={`col ${css.m1}`}>
                             <h1>Получите заказы уже сегодня</h1>
-                            <div className={css.p1}>
+                            <div>
                                 <p>Построй Ка - тут вы возьмете заказ по профилю, типу сложности и комфортной цене</p>
-                                <img src="/images/master1.png" alt="master" loading="lazy"/>
+                                <img src="/images/formasters/master1.png" alt="master" width="100" height="126" loading="lazy"/>
                             </div>
-                            <div className={css.p1}>
+                            <div>
                                 <p>Предложите свои услуги любому заказчику и найчнете работайте с ним напрямую</p>
-                                <img src="/images/master2.png" alt="master" loading="lazy"/>
+                                <img src="/images/formasters/master2.png" alt="master" width="141" height="130" loading="lazy"/>
                             </div>
-                            <div className={css.p1}>
-                                <style>{`
-                                     p:nth-child(2) {
-                                       color: #47CB43;
-                                     }                    
-                                `}</style>
+                            <div>
                                 <p>Абсолютно бесплатно и без комиссии</p>
-                                <img src="/images/master3.png" alt="master" loading="lazy"/>
+                                <img src="/images/formasters/master3.png" alt="master" width="100" height="113" loading="lazy"/>
                             </div>
                         </div>
-                        <img className={css.m2} src="/images/formasters.png" alt="formasters" loading="lazy"/>
+                        <img className={css.i2} src="/images/formasters/formasters.png" alt="formasters" width="572" height="477" loading="lazy"/>
                     </div>
                     <div className={`center ${css.b1}`}>
                         <Link href="/registration">
@@ -37,32 +32,55 @@ const ForMasters = () => {
                     </div>
                 </div>
 
-
-                <div className={css.g1}>
+                <div className={`max bet ${css.g1}`}>
                     <h1>Возьмите заказ на раз – два</h1>
                     <div className={`row bet`}>
                         <div>
                             <div className={`row bet ${css.ho}`}>
-                                <img src="/images/imgone.png" alt="one" loading="lazy"/>
+                                <img src="/images/formasters/imgone.png" alt="one" width="189" height="188" loading="lazy"/>
                                 <p><span>Зарегистрируйтесь</span>
                                     <br/>Заказчики хотят видеть, с кем они имеют дело. <br/>Чем больше информации вы
                                     заполните, тем чаще вас будут выбирать.</p>
-                                <span><img src="/images/Line.png" alt="line"/></span>
+                                <span><img src="/images/formasters/Line.png" width="322" height="101" alt="line" loading="lazy"/></span>
                             </div>
                             <span className={css.sp}>Р А З</span>
                         </div>
                         <div>
                             <div className={`row bet ${css.ho}`}>
-                                <img src="/images/imgtwo.png" alt="one" loading="lazy"/>
+                                <img src="/images/formasters/imgtwo.png" width="189" height="188" alt="one" loading="lazy"/>
                                 <p><span>Выбирайте интересные вам заказы</span>
-                                    <br/>ЗВам будут доступны все открытые по Вашему выбору заказы, по всем видам работ в рамках выбранного бюджета.</p>
-                                <span><img src="/images/Line.png" alt="line"/></span>
+                                    <br/>Вам будут доступны все открытые по Вашему выбору заказы, по всем видам работ в рамках выбранного бюджета.</p>
+                                <span><img src="/images/formasters/Line.png" width="322" height="101" alt="line" loading="lazy"/></span>
                             </div>
                             <span className={css.sp}>Д В А</span>
                         </div>
                     </div>
                 </div>
 
+                <div className={`row`}>
+                    <div className={`col bet`}>
+                        <h1>Получите заказы уже сегодня</h1>
+                        <div className={`col ${css.m3}`}>
+
+                            <div>
+                                <p><span>Биржа заказов</span><br/>Отправка предложений заказчикам в любой категории выбранной Вами без ограничений.</p>
+                            </div>
+                            <div>
+                                <p><span>Открытые чаты</span><br/>Обсуждение сроков, условий, цен.<br/>Обмен телефонами.<br/><br/> Договоренность с заказчиком о взаимодействии напрямую, вне сервиса.</p>
+                            </div>
+                    <div className={`center ${css.b1}`}>
+                        <Link href="/registration">
+                            <a>Зарегистрироваться</a>
+                        </Link>
+                </div>
+                        </div>
+                    </div>
+                    <img className={css.i2} src="/images/formasters/formasters.png" alt="formasters" width="572" height="477" loading="lazy"/>
+                </div>
+
+                <section className={css.i3}>
+                    <img src="/images/we.png" width="280" height="30" alt="one" loading="lazy"/>
+                </section>
 
             </main>
         </PublicLayout>
