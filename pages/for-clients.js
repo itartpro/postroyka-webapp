2f1a8a9d6a7a0d67d626fc8f1f0d0e00d42ac548
axlkov <ieffector@gmail.com>
2021-09-18 19:30:21 +0300

    homepages

---
+++ pages/for-clients.js
@@ -0,0 +1,14 @@
+import PublicLayout from "components/public/public-layout";
+import css from 'styles/for-clients.module.css';
+
+const ForClients = () => {
+    return (
+        <PublicLayout>
+            <main className={'row bet max center'}>
+                <h1>Здесь будет страница для клиентов, работаю над дизайном - Лёха</h1>
+            </main>
+        </PublicLayout>
+    )
+}
+
+export default ForClients
