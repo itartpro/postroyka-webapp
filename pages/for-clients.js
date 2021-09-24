11e054a84f50e0cd3fb17f69189719bb927ac3b4
axlkov <ieffector@gmail.com>
2021-09-24 22:00:37 +0300

    clients

--- pages/for-clients.js
+++ pages/for-clients.js
@@ -1,11 +1,44 @@
 import PublicLayout from "components/public/public-layout";
 import css from 'styles/for-clients.module.css';
+import {Hero} from "../components/public/home/hero";
+import Order from "../components/public/order";
+import {Button} from "../components/public/button";
 
 const ForClients = () => {
     return (
         <PublicLayout>
-            <main className={'row bet max center'}>
-                <h1>Здесь будет страница для клиентов, работаю над дизайном - Лёха</h1>
+            <main className={'col start'}>
+                <header className={`row ${css.hr1}`}><Hero/></header>
+
+                <div className={`row bet ${css.st1}`}>
+                    <div className="col start max">
+                        <h1>Построй Ка – не стройфирма, а портал мастеров и бригад</h1>
+                        <p>Это крупнейший <span>специализированный сайт для поиска подрядчиков</span> в России. Сотни
+                            заказчиков
+                            находят здесь мастеров ежедневно. И вот почему:</p>
+                    </div>
+                </div>
+                <section className={`row bet max`}>
+
+                    <div className={`row ${css.bl1}`}>
+                        <img src="images/clients/client1.png" alt="people"  width="281" height="142" loading="lazy"/>
+                        <div className={css.bl2}>
+                            <p>5 - 15 минут</p>
+                            <p>Среднее время ожидания первого предложения</p>
+                        </div>
+                    </div>
+                    <div className={`row ${css.bl1}`}>
+                        <img src="images/clients/client1.png" alt="people"  width="281" height="142" loading="lazy"/>
+                        <div className={css.bl2}>
+                            <p>5 - 15 минут</p>
+                            <p>Среднее время ожидания первого предложения</p>
+                        </div>
+                    </div>
+
+
+
+
+                </section>
             </main>
         </PublicLayout>
     )
