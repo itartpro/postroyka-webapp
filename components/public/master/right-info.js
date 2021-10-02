3b3bd0f964470ec2856766bab0d60a01c82bbb34
axlkov <ieffector@gmail.com>
2021-10-02 10:34:57 +0300

    chngs

--- components/public/master/right-info.js
+++ components/public/master/right-info.js
@@ -2,9 +2,11 @@ import css from './right-info.module.css';
 
 export const RightInfo = props => {
     return (
-        <aside className="col start">
-            <p>Расположение</p>
-            <b>Москва</b>
+        <aside className={`col start ${css.rig}`}>
+            <b>Расположение</b>
+            <p><span><img src="/images/profilermaster/geo.png" width="18" height="21" alt="geo" loading="lazy"/>
+</span>Кабардино-Балкарская Республика</p>
+            <blockquote>Выезд на объекты</blockquote>
         </aside>
     )
 }
