9ddd98d6467a6c1c34ec2d1823b5ef175b50c32c
leo-at-itartpro <mail@itart.pro>
2021-10-10 05:25:34 +0300

    stufff

---
+++ pages/temp-order.js
@@ -0,0 +1,16 @@
+import PublicLayout from "components/public/public-layout";
+import css from "styles/temp.module.css";
+
+const TempOrder = () => {
+    return (
+        <PublicLayout>
+            <main className="col start max">
+                <header className="row start">
+                    <h1>dfgdfgdgf</h1>
+                </header>
+            </main>
+        </PublicLayout>
+    )
+}
+
+export default TempOrder
