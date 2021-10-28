dd1726282994ae0fcb63d87c7c98c11e21d623b6
leo-at-itartpro <mail@itart.pro>
2021-10-28 14:23:55 +0300

    rust minifyer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    swcMinify: true,
     images: {deviceSizes: [414, 1366, 1920]},
     webpackDevMiddleware: config => {
         config.watchOptions = {poll: 1000, aggregateTimeout: 300};
