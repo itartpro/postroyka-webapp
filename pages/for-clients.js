58b5330c5560e09f7c9c627ea092fbe225075272
axlkov <ieffector@gmail.com>
2021-11-01 23:08:44 +0300

    img/h1

--- pages/for-clients.js
+++ pages/for-clients.js
@@ -36,7 +36,7 @@ const ForClients = () => {
                 <section className={`row bet max ${css.bl1}`}>
 
                     <div className={`row`}>
-                        <img src="images/clients/client1.png" alt="people" width="281" height="142" loading="lazy"/>
+                        <img src="images/clients/client1.png" alt="people" width="281" height="132" loading="lazy"/>
                         <div className={css.bl2}>
                             <p>5 - 15 минут</p>
                             <p>Среднее время ожидания первого предложения</p>
@@ -44,7 +44,7 @@ const ForClients = () => {
                     </div>
 
                     <div className={`row`}>
-                        <img src="images/clients/client2.png" alt="people" width="281" height="139" loading="lazy"/>
+                        <img src="images/clients/client2.png" alt="people" width="281" height="132" loading="lazy"/>
                         <div className={css.bl2}>
                             <p>До 60% экономии</p>
                             <p>За счет конкуренции мастеров и отсутствия комиссии</p>
@@ -60,7 +60,7 @@ const ForClients = () => {
                     </div>
 
                     <div className={`row`}>
-                        <img src="images/clients/client4.png" alt="people" width="281" height="138" loading="lazy"/>
+                        <img src="images/clients/client4.png" alt="people" width="281" height="132" loading="lazy"/>
                         <div className={css.bl2}>
                             <p>7 предложений</p>
                             <p>В среднем приходит на заказ</p>
@@ -68,7 +68,7 @@ const ForClients = () => {
                     </div>
 
                     <div className={`row`}>
-                        <img src="images/clients/client5.png" alt="people" width="281" height="150" loading="lazy"/>
+                        <img src="images/clients/client5.png" alt="people" width="281" height="132" loading="lazy"/>
                         <div className={css.bl2}>
                             <p>Конфиденциально</p>
                             <p>Сообщайте свои контакты только выбранным исполнителям</p>
@@ -76,7 +76,7 @@ const ForClients = () => {
                     </div>
 
                     <div className={`row`}>
-                        <img src="images/clients/client6.png" alt="people" width="281" height="139" loading="lazy"/>
+                        <img src="images/clients/client6.png" alt="people" width="281" height="132" loading="lazy"/>
                         <div className={css.bl2}>
                             <p>21 000 отзывов</p>
                             <p>Помогут сравнить исполнителей</p>
@@ -86,12 +86,12 @@ const ForClients = () => {
                 </section>
                 <div className={`row bet ${css.st1}`}>
                     <div className="col start max">
-                        <h1>Как найти мастера или бригаду на сайте Построй Ка?</h1>
+                        <h3>Как найти мастера или бригаду на сайте Построй Ке?</h3>
                         <p>От «мастера на час» до строительной компании — одинаково простая схема поиска, сравнения,
                             выбора и работы</p>
                     </div>
                 </div>
-                <section className={`row max bet ${css.iii}`}>
+                <section className={`row max bet`}>
                     <div className={css.sct}>
                         <div className={`row center bet`}>
                             <p>01</p>
@@ -169,13 +169,7 @@ const ForClients = () => {
                         <p>Примите работы и расскажите о своем взаимодействии с мастером.</p>
                     </div>
                 </section>
-                <div className={`row bet ${css.st1}`}>
-                    <div className="col start max">
-                        <h1>Как найти мастера или бригаду на сайте Построй Ка?</h1>
-                        <p>От «мастера на час» до строительной компании — одинаково простая схема поиска, сравнения,
-                            выбора и работы</p>
-                    </div>
-                </div>
+
                 <section className={`row bet max ${css.lin1}`}>
                     <div className={`col`}>
                         <img src="images/clients/im1.png" alt="people" width="350" height="185" loading="lazy"/>
@@ -185,26 +179,17 @@ const ForClients = () => {
                     </div>
                     <div className={`col`}>
                         <img src="images/clients/im2.png" alt="people" width="350" height="185" loading="lazy"/>
-                        <p><span>Работайте напрямую</span> <br/>Построй Ка — не посредник и не стройфирма, а
-                            информационный
-                            сайт.</p>
+                        <p><span>Выбирайте сами</span> <br/>Нет диспетчеров. Обсуждайте заказ непосредственно с подрядчиками, а не с колл-центром.</p>
                     </div>
                     <div className={`col`}>
                         <img src="images/clients/im3.png" alt="people" width="350" height="185" loading="lazy"/>
-                        <p><span>Работайте напрямую</span> <br/>Построй Ка — не посредник и не стройфирма, а
-                            информационный
-                            сайт.</p>
+                        <p><span>Экономьте до 60%</span> <br/>Конкуренция мастеров. Нет комиссии. Срез рынка от «суперэконом-» до «премиум-» класса.</p>
                     </div>
                 </section>
                 <section className={css.bg}>
                     <div className={`row max bet`}>
                         <div className={css.d2}>
-                            <h1>Найдите мастера под Ваши работы</h1>
-                            <div className={`row bet`}>
-                                <p>Мастеров онлайн: <span>312</span></p>
-                                <p>Заказов в неделю: <span>1 714</span></p>
-                                <p>Предложений в сутки: <span>615</span></p>
-                            </div>
+                            <h3>Найдите мастера под Ваши работы</h3>
                             <form onSubmit={handleSubmit}>
                                 <input type="text" name="title" value={query} onChange={handleParam(setQuery)} placeholder="Что требуется сделать?"/>
                                 <input type="submit" value="Найти мастера"/>
@@ -212,7 +197,13 @@ const ForClients = () => {
                             <p>Опишите, какие работы требуются. — Свободные мастера, которых заинтересует ваш заказ, пришлют свои предложения по ценам и срокам. — Вы сможете обсудить свой заказ и договориться о выезде на объект и работах напрямую с любым из ответивших исполнителей.</p>
                         </div>
                         <img src="images/clients/imgblock.jpg" alt="people" width="550" height="293" loading="lazy"/>
+                        <div className={`row center ${css.statis}`}>
+                            <p>Мастеров онлайн: <span>312</span></p>
+                            <p>Заказов в неделю: <span>1 714</span></p>
+                            <p>Предложений в сутки: <span>615</span></p>
+                        </div>
                     </div>
+
                 </section>
             </main>
         </PublicLayout>
